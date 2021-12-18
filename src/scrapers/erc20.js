2c1fd07e72f805bf3d86641a09e0890d0a2ce271
0xluminous <0xluminous@protonmail.com>
2021-12-17 23:08:27 -0800

    change logging

--- src/scrapers/erc20.js
+++ src/scrapers/erc20.js
@@ -1,30 +1,30 @@
 import debug from "debug"
-const log = debug("saitohodlers:scraper:erc20");
+//const log = debug("saitohodlers:scraper:erc20");
 import fetch from "node-fetch"
 
 const TOKEN_ADDRESS = "0xfa14fa6958401314851a17d6c5360ca29f74b57b";
 
 export default async function erc20() {
     const url = `https://etherscan.io/token/${TOKEN_ADDRESS}`;
-    log(`hitting ${url}`);
+    console.log(`hitting ${url}`);
 
     const response = await fetch(url);
-    log(`response ${response.status}`);
+    console.log(`response ${response.status}`);
     if (response.status !== 200) return null;
 
     const body = await response.text();
     if (!body) return null;
-    log(`body length ${body.length}`);
+    console.log(`body length ${body.length}`);
 
     try {
         const pattern = body.match(/number of holders (?<holders>[\d,]+) and updated information of the token/);
         const holders = Number(pattern.groups.holders.replace(",", ""));
         if (!Number.isInteger(holders) || holders <= 0) {
-            log(`invalid holder number returned`);
+            console.log(`invalid holder number returned`);
             return null;
         }
 
-        log(`holders ${holders}`);
+        console.log(`holders ${holders}`);
         return holders;
     } catch (e) {
         return null;
