6f256bfb6acff46e5068dc67d518701f2010aa37
0xluminous <0xluminous@protonmail.com>
2021-12-19 18:43:04 -0800

    update cache bust timeout

--- src/scrapers.js
+++ src/scrapers.js
@@ -89,7 +89,8 @@ export async function getAll() {
 
 // get all hodlers and update the one that most out of date
 let lastUpdateDate = Date.now();
-const cacheBustDuration = 60 * 60 * 12 * 1000; // 12 hours
+//const cacheBustDuration = 60 * 60 * 12 * 1000; // 12 hours
+const cacheBustDuration = 60 * 1000; // 60 seconds
 export async function getAllAndUpdateOne() {
     const diff = (Date.now() - lastUpdateDate);
     if (diff >= cacheBustDuration) {
