de4656a65fec139b5ab28ecdbf08d9cf6d9cec48
0xluminous <0xluminous@protonmail.com>
2022-06-05 18:53:19 -0700

    enable proxy for binance

--- src/scrapers.js
+++ src/scrapers.js
@@ -27,7 +27,7 @@ export const BinanceSmartChain = {
     protocol: "BEP-20",
     url: "https://bscscan.com/token/0x3c6dad0475d3a1696b359dc04c99fd401be134da",
     regex: /number of holders (?<holders>[\d,]+) and updated information of the token/,
-    proxy: false
+    proxy: true
 };
 
 export const BSV = {
